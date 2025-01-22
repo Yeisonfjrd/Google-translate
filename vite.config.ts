33b58159018c020d6f81b6a133d546592da9c4d4
Yeisonfjrd <andresfajardo1606@gmail.com>
2025-01-22 10:40:30 -0300

    Fix Vite config for Vitest and separate test configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    environment: 'happy-dom',
-    globals: true,
-    coverage: {
-      provider: 'v8',
-    },
-  },
 })
